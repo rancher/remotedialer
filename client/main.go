445f16c3f42f3868e3412b8ca5ec6ee9d5cfaaa3
johnlanni <ztywto@qq.com>
2021-05-19 11:33:50 +0800

    Let the remotedialer support proxy client, then we can use the dialer in the microservice architecture.

--- client/main.go
+++ client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"io"
 	"net/http"
 
 	"github.com/rancher/remotedialer"
@@ -10,13 +11,15 @@ import (
 )
 
 var (
-	addr  string
-	id    string
-	debug bool
+	addr    string
+	id      string
+	example string
+	debug   bool
 )
 
 func main() {
 	flag.StringVar(&addr, "connect", "ws://localhost:8123/connect", "Address to connect to")
+	flag.StringVar(&example, "example", "localhost:8124", "example server listen address")
 	flag.StringVar(&id, "id", "foo", "Client ID")
 	flag.BoolVar(&debug, "debug", true, "Debug logging")
 	flag.Parse()
@@ -28,6 +31,11 @@ func main() {
 	headers := http.Header{
 		"X-Tunnel-ID": []string{id},
 	}
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+	http.HandleFunc("/hello", helloHandler)
+	go http.ListenAndServe(example, nil)
 
 	remotedialer.ClientConnect(context.Background(), addr, headers, nil, func(string, string) bool { return true }, nil)
 }
