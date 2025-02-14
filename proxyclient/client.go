02038c52f91fc1e1605c9a4ddd9eb3970c517d74
Felipe C. Gehrke <felipe.gehrke@suse.com>
2025-02-14 15:00:37 -0300

    fixed imports

--- proxyclient/client.go
+++ proxyclient/client.go
@@ -11,14 +11,11 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/rancher/remotedialer"
-	"github.com/rancher/wrangler/v3/pkg/generated/controllers/core"
 	v1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
 
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
 )
 
 const (
